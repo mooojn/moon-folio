2c11c2f77df98c56f66ea3b47c6cf910738eda85
mooojn <moojntariq@gmail.com>
2025-10-06 22:13:08 +0500

    Add projects

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,42 @@
+import styles from "@/styles/projects.module.css";
+import projects from '../data/projects.json';
+
+export default function Projects() {
+  return (
+    <div id="projects" className={styles.projects}>
+      {projects.map((project, id) =>
+        <Card key={id} project={project} />
+      )}
+    </div>
+  );
+}
+
+function Card({ project }: { project: any }) {
+  return (
+    <div className={styles.project}>
+      <div className={styles.projectImage}>
+        <img src={project.image} alt={project.image} />
+      </div>
+      <h2>{project.category}</h2>
+      <h1>{project.title}</h1>
+      <p>{project.description}</p>
+
+      <ul className={styles.projectFeatures}>
+        {project.features.map((feature: any, id: any) =>
+          <li key={id}>✅ {feature}</li>
+        )}
+      </ul>
+      <ul className={styles.projectTechnologies}>
+        {project.technologies.map((technology: any, id: any) =>
+          <li key={id}>{technology}</li>
+        )}
+      </ul>
+
+      {project.actions.map((action: any, id: any) =>
+        <button key={id}><a href={action.link} target="_blank">{action.name}</a></button>
+      )}
+
+    </div>
+  );
+}
+
